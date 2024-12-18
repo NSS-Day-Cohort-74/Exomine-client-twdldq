790b9797c5d1ed7ac82195642c34eccdc01ac55c
Kai Swain <kaiferseain@icloud.com>
2024-12-18 10:09:50 -0600

    added modules

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+// Import our html functions
+
+// target our container const container = document.queryselector(#container)
+
+// make a render html function
+
+// set our html functions to variables
+
+//insert render into container.innerhtml
+
+// listen for a posted order and run  the render function
