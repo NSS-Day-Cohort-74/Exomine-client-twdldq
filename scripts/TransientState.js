6fc7f4c0d400cf165ca5f5012d6bb0fb11ce0d5d
Kai Swain <kaiferseain@icloud.com>
2024-12-19 15:41:31 -0600

    halfway through facilities module

--- scripts/TransientState.js
+++ scripts/TransientState.js
@@ -23,6 +23,7 @@ export const setMineral = (mineralId) => {
 
 
 export const purchaseMineral = () => {
+    
     /*
         Does the chosen governor's colony already own some of this mineral?
             - If yes, what should happen?
