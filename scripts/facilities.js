94b5d3a6f72a5dc3da1a91444934694eb832fb44
Kai Swain <kaiferseain@icloud.com>
2024-12-19 10:04:14 -0600

    facilities halfway done

--- scripts/facilities.js
+++ scripts/facilities.js
@@ -4,7 +4,17 @@
 
 // create a facility dropdown html function
 
+const facilityList = async () => {
+    const response = await fetch("http://localhost:8088/facilities")
+    const facilities = await response.json()
 
+    const html = "<selector><option>Facilities</option>"
+    for (const facility of facilities) {
+        html += `<option value=${facility.id}>${facility.name}</option>`
+    } 
+    html += "</section>"
+    return html
+}
 
 
 
